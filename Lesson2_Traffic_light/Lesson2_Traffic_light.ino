34a30f5a49990078ffb782894066ddc6c4e93c7e
vaclavpavlicek <vaclav@pavlicek.cz>
2014-11-01 10:37:53 +0100

    add Lesson2_Traffic_light.ino

---
+++ Lesson2_Traffic_light/Lesson2_Traffic_light.ino
@@ -0,0 +1,24 @@
+int redLed = 7;
+int yellowLed = 8;
+int greenLed = 9;
+
+  void setup(){
+  
+      pinMode(redLed,OUTPUT);
+      pinMode(yellowLed,OUTPUT);
+      pinMode(greenLed,OUTPUT);    
+    
+  }
+  
+  void loop(){
+  
+    digitalWrite(redLed,HIGH);    
+    delay(1000);
+    digitalWrite(redLed,LOW);
+    digitalWrite(yellowLed,HIGH);    
+    delay(1000);
+    digitalWrite(yellowLed,LOW);
+    digitalWrite(greenLed,HIGH);    
+    delay(1000);
+    digitalWrite(greenLed,LOW);    
+  }
